2ac85de42cbef4110b573329fc88b07270b27ff1
jor-ch <chtoh97@gmail.com>
2026-03-30 22:01:38 +0800

    Wrote constructor, destructor and attributes for abstract class LibraryItem
    Wrote constructor, destructor and attributes for abstract class LibraryItem

---
+++ include/items/LibraryItem.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <stdexcept>
+
+class LibraryItem
+{
+protected:
+    std::string id_;
+    std::string title_;
+    bool available_;
+    double dailyFine_;
+    int maxLoanDays_;
+
+public:
+    LibraryItem(const std::string &id, const std::string &title) : id_(id), title_(title), available_(true), dailyFine_(0.0), maxLoanDays_(0) {}
+
+    virtual ~LibraryItem() = default;
+};

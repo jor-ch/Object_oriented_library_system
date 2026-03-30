c77a61b82fa4c050d3540c09c4a8600e21326001
jor-ch <chtoh97@gmail.com>
2026-03-30 22:53:00 +0800

    initial testing of Book class to check if implementation is working properly
    initial testing of Book class to check if implementation is working properly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "Book.h"
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+    Book book("123", "The Best Title", "The Best Author", "123-4567-89", "Fiction");
+    std::cout << "Book Title: " << book.getTitle() << std::endl;
+    std::cout << "Author: " << book.getAuthor() << std::endl;
+    std::cout << "ISBN: " << book.getISBN() << std::endl;
+    std::cout << "Genre: " << book.getGenre() << std::endl;
+    return 0;
 }

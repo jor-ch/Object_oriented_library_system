151fa2ea91600dcfaf8f98f40b0a04775e545124
jor-ch <chtoh97@gmail.com>
2026-03-30 22:03:32 +0800

    wrote checkOut and returnItem void functions for LibraryItem abstract class
    wrote checkOut and returnItem void functions for LibraryItem abstract class

--- include/items/LibraryItem.h
+++ include/items/LibraryItem.h
@@ -15,4 +15,18 @@ public:
     LibraryItem(const std::string &id, const std::string &title) : id_(id), title_(title), available_(true), dailyFine_(0.0), maxLoanDays_(0) {}
 
     virtual ~LibraryItem() = default;
+
+    void checkOut()
+    {
+        if (!available_)
+        {
+            throw std::runtime_error("Item is not available for checkout.");
+        }
+        available_ = false;
+    }
+
+    void returnItem()
+    {
+        available_ = true;
+    }
 };

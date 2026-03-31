ddb60b8c1dba0141c954427cf78290764a715378
jor-ch <chtoh97@gmail.com>
2026-03-31 23:18:39 +0800

    added code to test functionality of Student class
    added code to test functionality of Student class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "Book.h"
+#include "Student.h"
 
 int main()
 {
+    // for testing of book and library item functionality
     Book book("123", "The Best Title", "The Best Author", "123-4567-89", "Fiction");
     std::cout << book.getDetails() << std::endl;
 
@@ -18,6 +20,7 @@ int main()
         std::cerr << "Error: " << e.what() << std::endl;
     }
 
+    // there should be an error here because the book is already checked out
     try
     {
         book.checkOut();
@@ -34,5 +37,15 @@ int main()
     std::cout << "Book returned successfully." << std::endl
               << "Is the book available? " << (book.isAvailable() ? "Yes" : "No") << std::endl;
     std::cout << "Calculating fine for 5 overdue days: $" << book.calculateFine(5) << std::endl;
+
+    // for testing of student and library patron functionality
+    Student student("ID456", "Best name", "best.name@example.com", "SID123456", "Computer Science");
+    std::cout << "Student Name: " << student.getName() << std::endl;
+    std::cout << "Student ID: " << student.getStudentId() << std::endl;
+    std::cout << "Student Major: " << student.getMajor() << std::endl;
+    std::cout << "Student Contact Info: " << student.getContactInfo() << std::endl;
+    std::cout << "ID: " << student.getId() << std::endl;
+    std::cout << "Is the student active? " << (student.isActive() ? "Yes" : "No") << std::endl;
+
     return 0;
 }

c2b1b29e224f44a64c334c7fc4fd03b0ac56788a
jor-ch <chtoh97@gmail.com>
2026-04-04 10:14:03 +0800

    Included code to test functionality of Checkout class
    Included code to test functionality of Checkout class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Book.h"
 #include "Student.h"
+#include <memory>
+#include "Checkout.h"
 
 int main()
 {
@@ -45,6 +47,26 @@ int main()
     std::cout << "Is the student active after deactivation? " << (student.isActive() ? "Yes" : "No") << std::endl;
     student.activate();
     std::cout << "Is the student active after reactivation? " << (student.isActive() ? "Yes" : "No") << std::endl;
+
+    // for testing of checkout transaction functionality
+    std::shared_ptr<LibraryItem> book1Ptr = std::make_shared<Book>("123", "The Best Title", "The Best Author", "123-4567-89", "Fiction");
+    std::shared_ptr<LibraryItem> book2Ptr = std::make_shared<Book>("1232", "The Best Title2", "The Best Author2", "123-4567-892", "Fiction");
+    std::shared_ptr<LibraryPatron> student1Ptr = std::make_shared<Student>("ID456", "Best name", "best.name@example.com", "SID123456", "Computer Science");
+    try
+    {
+        {
+            Checkout checkout(book1Ptr, student1Ptr);
+            std::cout << checkout.getDetails() << std::endl;
+            Checkout checkout2(book2Ptr, student1Ptr); // this should work because book2 is a different item and is available for checkout
+            std::cout << checkout2.getDetails() << std::endl;
+            Checkout checkout3(book1Ptr, student1Ptr); // this should throw an error because book1 is already checked out
+            std::cout << checkout3.getDetails() << std::endl;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
